590fa48a7ee0bf560ed941391f1182579f22cd35
Alexander Zolotukhin <alexanderzolotukhin96@gmail.com>
2023-12-21 20:38:22 -0600

    cleanup root dir, added req pip

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-from ibm_continuous_delivery.cd_toolchain_v2 import CdToolchainV2
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+#from ibm_continuous_delivery.cd_toolchain_v2 import CdToolchainV2
+#from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import requests
 
-authenticator = IAMAuthenticator('XXX')
-service = CdToolchainV2(authenticator=authenticator)
-responce = requests.get('https://api.us-south.devops.cloud.ibm.com/toolchain/v2')
-responce.json()
+r = requests.get
